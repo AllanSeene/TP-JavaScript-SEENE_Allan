a406730568a159ff89740839679192042643b6e0
Allan <allan.seene@outlook.fr>
2026-02-04 11:26:41 +0100

    TP5

--- monscript.js
+++ monscript.js
@@ -3,3 +3,18 @@ let numJour = "3";
 let nomMois = "Fevrier";
 let isWinter = true;
 console.log(nomJour , numJour , nomMois , isWinter);
+let tabJours = ["Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche"];
+let tabMois = ["Janvier","Fevrier","Mars","Avril","Mai","Juin","Juillet","Aout","Septembre","Octobre","Novembre","Decembre"];
+console.log(tabJours[2], tabMois[1]);
+
+let maDate={
+    jour : tabJours[2],
+    mois : tabMois[1],
+};
+let maDateV2={
+    jour : nomJour,
+    mois : nomMois,
+};
+
+console.log(maDate.jour, maDate.mois);
+console.log(maDateV2.jour, maDateV2.mois);
