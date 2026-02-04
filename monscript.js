91dfda981ba39838eb0240206fb47b8bb37b5876
Allan <allan.seene@outlook.fr>
2026-02-04 11:21:50 +0100

    TP4

--- monscript.js
+++ monscript.js
@@ -1,3 +1,5 @@
-console.log('Hello world !');
-let reponse = prompt('Quelle est ta série préférée ?');
-console.log(reponse);
+let nomJour = "Mercredi";
+let numJour = "3";
+let nomMois = "Fevrier";
+let isWinter = true;
+console.log(nomJour , numJour , nomMois , isWinter);
