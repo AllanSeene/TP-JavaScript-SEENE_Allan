b0c6ab597a74bd6efdbc6d70ff042a1c7a2e7d5b
Allan <allan.seene@outlook.fr>
2026-02-04 11:32:02 +0100

    TP6

--- monscript.js
+++ monscript.js
@@ -18,3 +18,20 @@ let maDateV2={
 
 console.log(maDate.jour, maDate.mois);
 console.log(maDateV2.jour, maDateV2.mois);
+
+let nbr1;
+let nbr2;
+let nbr3;
+let nbr4;
+nbr1=3;
+nbr2=7;
+nbr3=8;
+nbr4=21;
+console.log(nbr1 + nbr2);
+console.log(nbr4 - nbr3);
+nbr2 = nbr1 * nbr2;
+console.log(nbr2);
+nbr2 ++;
+console.log(nbr2);
+console.log(nbr4 / nbr1);
+console.log(nbr4 % nbr1);
