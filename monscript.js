c5b2172c15698bffba38d7fa43fe1809c9a86c35
Allan <allan.seene@outlook.fr>
2026-02-25 11:33:56 +0100

    TP13

--- monscript.js
+++ monscript.js
@@ -151,5 +151,17 @@ console.log(result);
 
 
 
+/* Toutes les variables */
+let li = document.querySelector("#listeCourses li");
 
+
+/* Tous les évènements */
+li.addEventListener('click', ()=>{
+  li.style.backgroundColor = "lightblue";
+  li.style.textDecoration = "line-through";
+})
+
+
+/* Les fonctions */
+// A voir plus tard
 
